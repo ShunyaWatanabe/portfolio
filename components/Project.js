73a194c3f4d3b3af9337eb707d8e3b3520cbba39
shunyawatanabe <s.watte3212@gmail.com>
2020-06-24 23:43:47 +0400

    Fix typo

--- components/Project.js
+++ components/Project.js
@@ -1,4 +1,4 @@
-import styles from './project.module.scss';
+import styles from './Project.module.scss';
 import ContactLinks from 'components/ContactLinks';
 import ProjectTag from 'components/ProjectTag';
 
